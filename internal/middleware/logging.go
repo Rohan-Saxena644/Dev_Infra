c220948fdacc14410ac3272fa1a1460e774492cc
Rohan-Saxena644 <rohsax83@gmail.com>
2026-06-24 23:22:38 +0530

    added structured logging using slog

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -2,13 +2,23 @@ package middleware
 
 import(
 	"net/http"
-	"log"
+	"log/slog"
+	"time"
 )
 
 
 func Logging(next http.Handler)(http.Handler){
 	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
-		log.Printf("%s %s",r.Method,r.URL.Path)
+
+		start := time.Now()
+
 		next.ServeHTTP(w,r)
+
+		slog.Info(
+			"request compelted",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"duration", time.Since(start),
+		)
 	})
 }
